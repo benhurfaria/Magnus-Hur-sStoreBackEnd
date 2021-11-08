19513f665fd80d6ec26e590dc8994e3ecae6fc3e
Audamon <joaotamagno@gmail.com>
2021-11-08 14:49:24 -0300

    teste

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import connection from './database.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/teste', async (req, res) => {
+    console.log('ok');
+    try {
+        res.send(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+
+    }
+}
+)
+
+
+
+
+
+
+
+export default app;
