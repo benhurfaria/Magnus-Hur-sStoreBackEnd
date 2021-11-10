f3da4ae7d2a71b0909f7546923ba793dd7bbc538
Ben Hur Faria <benhurfaria3@gmail.com>
2021-11-09 21:40:35 -0300

    ajustes

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,7 +4,7 @@ const signUpSchema = joi.object({
     name: joi.string().min(3).required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
-})
+});
 
 export{
     signUpSchema
