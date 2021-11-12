5b7b70ed368663d4703f27104cfeecfc4827fe39
Ben Hur Faria <benhurfaria3@gmail.com>
2021-11-12 00:49:02 -0300

    refatoracao basica

--- src/app.js
+++ src/app.js
@@ -6,28 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/teste', async (req, res) => {
-  try {
-    res.status(200).send('ola');
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
-
-app.get('/oi', async (req, res) => {
-  try {
-    res.status(200).send('ola mundo');
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
-
-app.get('/xablau', (req, res) => {
-  res.sendStatus(500);
-});
-
 app.post('/sign-in', signIn);
 
 export default app;
